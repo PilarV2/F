1baa6097fa179133e71194cf848405c21db2b5a7
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-03-13 09:41:39 +0700

    Update buktiprem.js

--- plugins/buktiprem.js
+++ plugins/buktiprem.js
@@ -1,6 +1,6 @@
 let handler = async(m, { conn, text }) => {
     const laporan = `*_Hallo Bos Ada Yang Mau Beli Prem Nih*_\nNomor : wa.me/${m.sender.split`@`[0]}`
-    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6281515860089@s.whatsapp.net'))
+    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6289625556161@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(`✅ Bukti Anda Telah Terkirim\nMohon Tunggu Info Selanjutnya\n\nDan Pastikan Anda Telah Membayarnya\nJika Blm Ada Respon, Chat Ke Owner Ku Yah\n${global.pemilikasli}`)
 }
