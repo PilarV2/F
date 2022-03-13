e32f1e29a7713543ca163877d86791761bcd775e
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-03-13 17:14:55 +0700

    Update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -16,7 +16,7 @@ Waktu *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}hint untuk bantuan
 `.trim()
   conn.tebakgambar[id] = [
-    await conn.sendButtonImg(m.chat, json.img, caption, '© Nekohime', 'Bantuan', '.hint', m),
+    await conn.sendButtonImg(m.chat, json.img, caption, '© ZeusBotz', 'Bantuan', '.hint', m),
     json, poin,
     setTimeout(() => {
       if (conn.tebakgambar[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© ZeusBotz', 'Tebak Gambar', '.tebakgambar', conn.tebakgambar[id][0])
