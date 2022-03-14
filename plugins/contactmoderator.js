2b93b8b4baa9ec9c3b263f6db709fe39644d8bcd
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-03-14 09:10:24 +0700

    Update contactmoderator.js

--- plugins/contactmoderator.js
+++ plugins/contactmoderator.js
@@ -1,8 +1,6 @@
 let handler = function (m) {
   // this.fakeReply(m.chat, 'This is my owner', '6281319859673@s.whatsapp.net', 'Owner')
-  this.sendContact(m.chat, '6285705034965', 'Moderator 1', m); //Krisna
-  this.sendContact(m.chat, '6281388985270', 'Moderator 2', m); // Daffa
-  this.sendContact(m.chat, '6289691596342', 'Moderator 3', m); // Rija
+  this.sendContact(m.chat, '6282287750102', 'Moderator 1', m); //AlpinGey
 };
 handler.help = ['moderator'];
 handler.tags = ['info'];
